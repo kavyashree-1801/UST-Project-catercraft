4814c4438ed2cb5c374d7e76b394920a4f147b23
kavyashree-1801 <kavyashreedmmohan@gmail.com>
2025-12-24 16:27:11 +0530

    Delete catercraft directory

--- catercraft/js/order_success.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Order Success Page Loaded');
-
-    // Optional auto-redirect after 10 seconds
-    // setTimeout(() => {
-    //     window.location.href = 'products.php';
-    // }, 10000);
-});
